f8428739c67fb6d039fbeccfa622fdf6f367dc33
Zamur650 <Zamur_yt@mail.ru>
2021-08-31 10:35:26 +0700

    Block added.
    yep.

--- app.js
+++ app.js
@@ -1,54 +1,66 @@
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+)
 
 const renderer = new THREE.WebGLRenderer()
-renderer.shadowMap.enabled = true;
+renderer.shadowMap.enabled = true
 renderer.setSize(window.innerWidth, window.innerHeight)
 
-let game = document.querySelector('.game')
+var game = document.querySelector('.game')
 game.appendChild(renderer.domElement)
 
 // Settings
-let levelLength = 100
+var levelLength = 100
 
 // Plane
-const geometryPlane = new THREE.BoxGeometry(1000, 1, 3)
+const geometryPlane = new THREE.BoxGeometry(1, 1, 1)
 const materialPlane = new THREE.MeshPhongMaterial({
-  color: 0xFFFFFF
+	color: 0xffffff,
 })
-let plane = new THREE.Mesh(geometryPlane, materialPlane)
+var plane = new THREE.Mesh(geometryPlane, materialPlane)
+plane.scale.x = 1000
+plane.scale.z = 3
 plane.position.y = -0.5
 scene.add(plane)
 
 // Character
-const geometryBox = new THREE.BoxGeometry(1, 1, 1)
-const materialBox = new THREE.MeshPhongMaterial({
-  color: 0xFFFFFF
+const characterBox = new THREE.BoxGeometry(1, 1, 1)
+const characterMaterial = new THREE.MeshPhongMaterial({
+	color: 0xffffff,
 })
-let character = new THREE.Mesh(geometryBox, materialBox)
+var character = new THREE.Mesh(characterBox, characterMaterial)
 character.position.y = 0.5
 character.x_v = 0
 character.y_v = 0
 character.landed = true
-character.gravity = 0.001
+character.gravity = 0.01
 
 scene.add(character)
 
 // Block
-/*let enemy = new THREE.Mesh(geometryBox, materialBox)
+const enemyGeometry = new THREE.BoxGeometry(1, 1, 1)
+const enemyMaterial = new THREE.MeshPhongMaterial({
+	color: 0xffffff,
+})
+var enemy = new THREE.Mesh(enemyGeometry, enemyMaterial)
 
+enemy.scale.y = 2
 enemy.position.x = 3
-enemy.position.y = 0.5
+enemy.position.y = 1
 
-scene.add(enemy)*/
+scene.add(enemy)
 
 // Light
-const pointLight = new THREE.PointLight(0x0000FF, 1, 50)
+const pointLight = new THREE.PointLight(0x0000ff, 1, 50)
 pointLight.position.set(3, 3, 3)
 scene.add(pointLight)
 
 // Light
-const pointLight1 = new THREE.PointLight(0xFF0000, 1, 50)
+const pointLight1 = new THREE.PointLight(0xff0000, 1, 50)
 pointLight1.position.set(-3, 3, -3)
 scene.add(pointLight1)
 
@@ -56,87 +68,93 @@ scene.add(pointLight1)
 camera.rotation.y = 105
 camera.rotation.y = 135
 
-scene.add( camera )
+scene.add(camera)
 
 // Progress bar
-let progress = 0
-let progressBar = document.getElementById('progressBar')
+var progress = 0
+var progressBar = document.getElementById('progressBar')
 
 // Init
 render()
-gameController()
+setInterval(gameController, 16)
 window.focus()
 
 // Music
-let music = new Audio('assets/music/i_like_cute_girls.mp3')
+var music = new Audio('assets/music/i_like_cute_girls.mp3')
 music.play()
 
 // Fix wrong size of a game when resized
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
 
-  renderer.setSize(window.innerWidth, window.innerHeight)
+	renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
 window.addEventListener('resize', onWindowResize, false)
 
 // Key Controller
 function keyDown(data) {
-  if ((data.code == 'Space' || data.code == 'Click') && character.landed == true) {
-    character.landed = false
-    character.y_v += 0.05
-  }
+	if (
+		(data.code == 'Space' || data.code == 'Click') &&
+		character.landed == true
+	) {
+		character.landed = false
+		character.y_v += 0.21
+	}
 }
-  
+
 window.addEventListener('keydown', keyDown)
 
 function gameController() {
-  // Camera
-  camera.lookAt(character.position)
+	// Camera
+	camera.lookAt(character.position)
 
-  camera.position.x = character.position.x - 2
-  camera.position.y = character.position.y + 2
-  camera.position.z = character.position.z + 3  
+	camera.position.x = character.position.x - 2
+	camera.position.y = character.position.y + 2
+	camera.position.z = character.position.z + 3
 
-  // Progress bar
-  progress = character.position.x / levelLength * progressBar.max
-  progressBar.value = progress
+	// Progress bar
+	progress = (character.position.x / levelLength) * progressBar.max
+	progressBar.value = progress
 
-  // characterMoveController
-  character.position.x += 0.01
+	// characterMoveController
+	character.position.x += 0.01
 
-  character.landed = false
+	character.landed = false
 
-  scene.children.forEach(object => {
-    if (object == character || object == camera || object.type == 'PointLight') return
+	scene.children.forEach((object) => {
+		if (object == character || object == camera || object.type == 'PointLight')
+			return
 
-    if ( // ! End the engine
-      /*object.position.x - object.scale.x / 2 > character.position.x + character.scale.x / 2 &&
-      object.position.x + object.scale.x / 2 < character.position.x - character.scale.x / 2 &&*/
-      object.position.y + object.scale.y / 2 >= character.position.y - character.scale.y / 2/* &&  
-      object.position.y - object.scale.y / 2 < character.position.y + character.scale.y / 2*/
-    ) {
-      character.landed = true
-      character.rotation.z = 0
-      character.position.y = object.position.y + (object.scale.y + character.scale.y) / 2
-      if (character.y_v <= 0) character.y_v = 0  
-    }
-  })
+		if (
+			object.position.x + object.scale.x / 2 <=
+				character.position.x - character.scale.x / 2 ||
+			object.position.x - object.scale.x / 2 >=
+				character.position.x + character.scale.x / 2 ||
+			object.position.y + object.scale.y / 2 <=
+				character.position.y - character.scale.y / 2 ||
+			object.position.y - object.scale.y / 2 >=
+				character.position.y + character.scale.y / 2
+		)
+			return
 
-  if (character.landed == false) {
-    character.rotation.z -= 0.016
-    character.y_v -= character.gravity
-  }
+		if (character.y_v <= 0) character.y_v = 0
 
-  character.position.y += character.y_v
+		character.landed = true
+	})
 
-  requestAnimationFrame(gameController)
+	character.rotation.z = character.rotation.z - 0.04
+
+	if (!character.landed) character.y_v -= character.gravity
+	if (character.landed) character.rotation.z = 0
+
+	character.position.y += character.y_v
 }
 
 // Render world
 function render() {
-  renderer.render(scene, camera)
+	renderer.render(scene, camera)
 
-  requestAnimationFrame(render)
+	requestAnimationFrame(render)
 }
